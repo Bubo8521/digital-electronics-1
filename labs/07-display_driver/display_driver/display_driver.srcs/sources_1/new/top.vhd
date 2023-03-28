ced5d3c7418479e34a2aec9d4e0d36d3fb86b051
Bubo8521 <124887713+Bubo8521@users.noreply.github.com>
2023-03-28 17:52:41 +0200

    Update top.vhd

--- labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
+++ labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
@@ -33,7 +33,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
     Port ( CLK100MHZ : in STD_LOGIC;
-           SW : in STD_LOGIC_VECTOR (15 downto 0);
+           SW : in STD_LOGIC_VECTOR (31 downto 0);
            CA : out STD_LOGIC;
            CB : out STD_LOGIC;
            CC : out STD_LOGIC;
@@ -54,18 +54,39 @@ architecture behavioral of top is
 begin
 
   --------------------------------------------------------
-  -- Instance (copy) of driver_7seg_4digits entity
+  -- Instance (copy) of driver_7seg_8digits entity
   --------------------------------------------------------
-  driver_seg_4 : entity work.driver_7seg_4digits
+  driver_seg_8 : entity work.driver_7seg_8digits
       port map (
           clk      => CLK100MHZ,
           rst      => BTNC,
+          
+          data7(3) => SW(15),
+          data7(2) => SW(14),
+          data7(1) => SW(13),
+          data7(0) => SW(12),
+
+          -- MAP OTHER DATA INPUTS TO ON-BOARD SWITCHES 11:4 HERE
+          data6(3) => SW(11),
+          data6(2) => SW(10),
+          data6(1) => SW(9),
+          data6(0) => SW(8),
+          
+          data5(3) => SW(7),
+          data5(2) => SW(6),
+          data5(1) => SW(5),
+          data5(0) => SW(4),
+          
+          data4(3) => SW(3),
+          data4(2) => SW(2),
+          data4(1) => SW(1),
+          data4(0) => SW(0),
+          
           data3(3) => SW(15),
           data3(2) => SW(14),
           data3(1) => SW(13),
           data3(0) => SW(12),
 
-          -- MAP OTHER DATA INPUTS TO ON-BOARD SWITCHES 11:4 HERE
           data2(3) => SW(11),
           data2(2) => SW(10),
           data2(1) => SW(9),
@@ -91,17 +112,15 @@ begin
           seg(0) => CG,
 
           -- DECIMAL POINT
-          dp_vect => "0111",
+          dp_vect => "11111111",
           dp      => DP,
 
           -- DIGITS
-          dig(3 downto 0) => AN(3 downto 0)
+          dig(7 downto 0) => AN(7 downto 0)
       );
 
   --------------------------------------------------------
   -- Other settings
   --------------------------------------------------------
-  -- Disconnect the top four digits of the 7-segment display
-  AN(7 downto 4) <= b"1111";
-
+ 
 end architecture behavioral;
